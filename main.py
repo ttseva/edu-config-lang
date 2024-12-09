cbd8ded51965bfc48d6a4ccb27aed748078d6fdd
coder2 <coder2@email.com>
2024-12-09 13:01:33 +0300

    add parse

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import json
+import argparse
+import sys
 
+def json_to_config(json_data):
+ return
+
+def main():
+    parser = argparse.ArgumentParser(description='Преобразование JSON в учебный конфигурационный язык.')
+    parser.add_argument('--input', help='Путь к входному JSON файлу')
+    parser.add_argument('--output', help='Путь к выходному файлу конфигурационного языка')
+
+    args = parser.parse_args()
+
+    try:
+        with open(args.input, 'r', encoding='utf-8') as infile:
+            json_data = json.load(infile)
+    except (FileNotFoundError, json.JSONDecodeError) as err:
+        print(f"Ошибка чтения: {err}")
+        return
+
+    try:
+        config_data = json_to_config(json_data)
+    except ValueError as err:
+        print(f"Ошибка конвертации: {err}")
+        return
+
+    try:
+        with open(args.output, 'w', encoding='utf-8') as outfile:
+            outfile.write(config_data)
+    except IOError as err:
+        print(f"Ошибка записи: {err}")
+        return
+
+    print(f"Успешно! Конвертация записана в {args.output}")
+
+
+if __name__ == "__main__":
+    main()
